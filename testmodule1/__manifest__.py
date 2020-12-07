2abe1f36eef93d40054fa8ca8526b7ff9ef74994
ЯкушевскийВВ <40803132+Vakatr@users.noreply.github.com>
2020-12-07 14:16:56 +0300

    123

--- testmodule1/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    'name': 'Lab2Test',
-    'version': '1.0',
-    'category': 'TestLab',
-    'description': """
-Модуль для 2 лаб. работы
-""",
-}
--- testmodule1/modules/models.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-
-class NewModule(models.Model):
-_name = 'feedback.form'
-_rec_name = 'name'
-_description = 'New Description'
-
-name = fields.Char()
